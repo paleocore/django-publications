22471997e16b13079979ab122cf9fe0fbecbcc01
dennereed <denne.reed@gmail.com>
2021-02-26 16:06:14 -0800

    Add doi links and pdf links to list view

--- publications/admin/publicationadmin.py
+++ publications/admin/publicationadmin.py
@@ -3,49 +3,64 @@ __author__ = 'Lucas Theis <lucas@theis.io>'
 __docformat__ = 'epytext'
 
 from django.contrib import admin
+from django.utils.html import format_html
+
 try:
-	from django.conf.urls import url
+    from django.conf.urls import url
 except ImportError:
-	from django.conf.urls.defaults import url
+    from django.conf.urls.defaults import url
 from publications.models import CustomLink, CustomFile
 
 import publications.admin_views
 
+
 class CustomLinkInline(admin.StackedInline):
-	model = CustomLink
-	extra = 1
-	max_num = 5
+    model = CustomLink
+    extra = 1
+    max_num = 5
 
 
 class CustomFileInline(admin.StackedInline):
-	model = CustomFile
-	extra = 1
-	max_num = 5
+    model = CustomFile
+    extra = 1
+    max_num = 5
 
 
 class PublicationAdmin(admin.ModelAdmin):
-	list_display = ('citekey', 'type', 'first_author', 'title', 'type', 'year', 'journal_or_book_title')
-	list_display_links = ('title',)
-	change_list_template = 'admin/publications/publication_change_list.html'
-	search_fields = ('title', 'journal', 'authors', 'keywords', 'year')
-	fieldsets = (
-		(None, {'fields':
-			('type', 'title', 'authors', 'year', 'month')}),
-		(None, {'fields':
-			('journal', 'book_title', 'publisher', 'institution', 'volume', 'number', 'pages')}),
-		(None, {'fields':
-			('citekey', 'keywords', 'url', 'code', 'pdf', 'doi', 'isbn', 'note', 'external')}),
-		(None, {'fields':
-			('abstract',)}),
-		(None, {'fields':
-			('image', 'thumbnail')}),
-		(None, {'fields':
-			('lists',)}),
-	)
-	inlines = [CustomLinkInline, CustomFileInline]
+    list_display = ('citekey', 'type', 'first_author', 'title', 'type', 'year', 'journal_or_book_title',
+                    'doi_link', 'pdf')
+    list_display_links = ('title',)
+    change_list_template = 'admin/publications/publication_change_list.html'
+    search_fields = ('title', 'journal', 'authors', 'keywords', 'year')
+    fieldsets = (
+        (None, {'fields':
+                    ('type', 'title', 'authors', 'year', 'month')}),
+        (None, {'fields':
+                    ('journal', 'book_title', 'publisher', 'institution', 'volume', 'number', 'pages')}),
+        (None, {'fields':
+                    ('citekey', 'keywords', 'url', 'code', 'pdf', 'doi', 'isbn', 'note', 'external')}),
+        (None, {'fields':
+                    ('abstract',)}),
+        (None, {'fields':
+                    ('image', 'thumbnail')}),
+        (None, {'fields':
+                    ('lists',)}),
+    )
+    inlines = [CustomLinkInline, CustomFileInline]
 
-	def get_urls(self):
-		return [
-				url(r'^import_bibtex/$', publications.admin_views.import_bibtex,
-					name='publications_publication_import_bibtex'),
-			] + super(PublicationAdmin, self).get_urls()
+    def get_urls(self):
+        return [
+                   url(r'^import_bibtex/$', publications.admin_views.import_bibtex,
+                       name='publications_publication_import_bibtex'),
+               ] + super(PublicationAdmin, self).get_urls()
+
+    def doi_link(self, obj):
+        if obj.doi:
+            # context_url = reverse('admin:origins_context_change', args=(obj.context.id,))
+            doi_url = "https://doi.org/{}".format(obj.doi)
+            return format_html('<a href={}>{}</a>'.format(doi_url, doi_url))
+        else:
+            return None
+
+    doi_link.admin_order_field = 'doi'
+    doi_link.short_description = 'doi'
