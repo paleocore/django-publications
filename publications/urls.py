b240e699b6ab6a1959951e12bfc26402fc66ea7c
dennereed <denne.reed@gmail.com>
2021-07-07 21:57:18 -0700

    updae urls

--- publications/urls.py
+++ publications/urls.py
@@ -2,6 +2,7 @@ __license__ = 'MIT License <http://www.opensource.org/licenses/mit-license.php>'
 __author__ = 'Lucas Theis <lucas@theis.io>'
 __docformat__ = 'epytext'
 
+from django.urls import include, path, re_path
 try:
     from django.conf.urls import url
 except ImportError:
@@ -11,11 +12,17 @@ from publications import views
 
 app_name = 'publications'
 urlpatterns = [
-    url(r'^$', views.year, name='index'),
-    url(r'^(?P<publication_id>\d+)/$', views.id, name='id'),
-    url(r'^year/(?P<year>\d+)/$', views.year, name='year'),
-    url(r'^tag/(?P<keyword>.+)/$', views.keyword, name='keyword'),
-    url(r'^list/(?P<list>.+)/$', views.list, name='list'),
-    url(r'^unapi/$', views.unapi, name='unapi'),
-    url(r'^(?P<name>.+)/$', views.author, name='author'),
+    path('', views.year, name='index'),
+    path('year/<int:year>/', views.year, name='year'),
+    path('<int:pk>/', views.id, name='id'),
+    path('tag/<keyword>/', views.keyword, name='keyword'),
+    path('list/<list>/', views.list, name='list'),
+    path('unapi/', views.unapi, name='unapi'),
+    path('<name>/', views.author, name='author'),
+    #re_path(r'(?P<publication_id>\d+)/', views.id, name='id'),
+    #path('year/(?P<year>\d+)/', views.year, name='year'),
+    #re_path(r'^tag/(?P<keyword>.+)/$', views.keyword, name='keyword'),
+    #re_path(r'^list/(?P<list>.+)/$', views.list, name='list'),
+    #re_path(r'^unapi/$', views.unapi, name='unapi'),
+    #e_path(r'^(?P<name>.+)/$', views.author, name='author'),
 ]
