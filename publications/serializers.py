690b7d8e085b057adccc101a0919d6e92856bfc7
dennereed <denne.reed@gmail.com>
2025-11-26 07:53:40 -0800

    Added render_reference method to Publication class and added Reference serializer that returns a bibliographic reference.

--- publications/serializers.py
+++ publications/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from publications.models import Publication
 
 class ReferenceSerializer(serializers.ModelSerializer):
 	bibliographic_reference = serializers.SerializerMethodField()
